9655da80b709b1f0b4e5c0a28e74be6b79944603
DanielAiresOficial <daniel.99781048@gmail.com>
2023-06-11 11:34:17 -0300

    OK

--- HackCaixa/Simular_Emprestimo/models.py
+++ HackCaixa/Simular_Emprestimo/models.py
@@ -8,3 +8,21 @@ class Produto(models.Model):
     NU_MAXIMO_MESES = models.SmallIntegerField(null=True)
     VR_MINIMO = models.DecimalField(max_digits=18, decimal_places=2)
     VR_MAXIMO = models.DecimalField(max_digits=18, decimal_places=2, null=True)
+
+"""
+ from .models import Produto
+
+produto1 = Produto(CO_PRODUTO=1, NO_PRODUTO='Produto 1', PC_TAXA_JUROS=0.017900000, NU_MINIMO_MESES=0, NU_MAXIMO_MESES=24, VR_MINIMO=200.00, VR_MAXIMO=10000.00)
+produto1.save()
+
+produto2 = Produto(CO_PRODUTO=2, NO_PRODUTO='Produto 2', PC_TAXA_JUROS=0.017500000, NU_MINIMO_MESES=25, NU_MAXIMO_MESES=48, VR_MINIMO=10001.00, VR_MAXIMO=100000.00)
+produto2.save()
+
+produto3 = Produto(CO_PRODUTO=3, NO_PRODUTO='Produto 3', PC_TAXA_JUROS=0.018200000, NU_MINIMO_MESES=49, NU_MAXIMO_MESES=96, VR_MINIMO=100000.01, VR_MAXIMO=1000000.00)
+produto3.save()
+
+produto4 = Produto(CO_PRODUTO=4, NO_PRODUTO='Produto 4', PC_TAXA_JUROS=0.015100000, NU_MINIMO_MESES=96, NU_MAXIMO_MESES=None, VR_MINIMO=1000000.01, VR_MAXIMO=None)
+produto4.save()
+
+"""
+
--- HackCaixa/Simular_Emprestimo/views.py
+++ HackCaixa/Simular_Emprestimo/views.py
@@ -1,3 +1,87 @@
 from django.shortcuts import render
+from azure.eventhub import EventHubProducerClient, EventData
+from .models import Produto
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+def envie_eventhub(request):
+    # Verificação de Requisicao POST
+    if request.method == 'POST':
+        # Obter request
+        mensagem = request.POST.get('mensagem')
+
+        # Definição da conexão
+        conn_str = "Endpoint=sb://eventhack.servicebus.windows.net/;SharedAccessKeyName=hack;SharedAccessKey=HeHeVaVqyVkntO2FnjQcs2Ilh/4MUDo4y+AEhKp8z+g=;EntityPath=simulacoes"
+
+        # Instanciação do cliente EventHubProducerClient
+        producer = EventHubProducerClient.from_connection_string(conn_str)
+
+        # lote de eventos
+        batch = producer.create_batch()
+
+        # Add mensagem ao lote de eventos
+        batch.add(EventData(mensagem))
+
+        # Envie o lote de eventos para o Event Hub
+        producer.send_batch(batch)
+
+        # Feche a conexão com o Event Hub
+        producer.close()
+
+        # Renderize a resposta para o usuário
+        return render(request, 'enviar_mensagem.html', {'mensagem_enviada': True})
+
+    # Se não for uma requisição POST, renderize o formulário de envio de mensagem
+    return render(request, 'enviar_mensagem.html')
+
+def calcular_tabela_price(valor, prazo, taxa_juros):
+    PMT = (valor * taxa_juros) / (1 - (1 + taxa_juros) ** -prazo)
+    tabela_price = []
+    saldo_devedor = valor
+    for i in range(prazo):
+        juros = saldo_devedor * taxa_juros
+        amortizacao = PMT - juros
+        saldo_devedor -= amortizacao
+        tabela_price.append((PMT, juros, amortizacao, saldo_devedor))
+    return tabela_price
+
+def calcular_tabela_sac(valor, prazo, taxa_juros):
+    saldo_devedor = valor
+    amortizacao = valor / prazo
+    tabela_sac = []
+    for i in range(prazo):
+        juros = saldo_devedor * taxa_juros
+        parcela = amortizacao + juros
+        saldo_devedor -= amortizacao
+        tabela_sac.append((amortizacao, saldo_devedor, juros, parcela))
+    return tabela_sac
+
+def simular_emprestimo(request):
+    if request.method == 'POST':
+        valor = request.POST['valor']
+        prazo = request.POST['prazo']
+        
+        # Consultar o banco de dados para encontrar o produto com a menor taxa de juros
+        produto = Produto.objects.filter(VR_MINIMO__lt=valor, VR_MAXIMO__gt=valor).order_by('PC_TAXA_JUROS').first()
+
+        if produto:
+            # Calcular a tabela Price
+            tabela_price = calcular_tabela_price(valor, prazo, produto.PC_TAXA_JUROS)
+    
+            # Calcular a tabela SAC
+            tabela_sac = calcular_tabela_sac(valor, prazo, produto.PC_TAXA_JUROS)
+    
+              # Criar o dicionário de contexto com os dados do resultado
+            context = {
+                'produto': produto,
+                'valor': valor,
+                'prazo': prazo,
+                'tabela_price': tabela_price,
+                'tabela_sac': tabela_sac
+            }
+    
+            # Renderizar o template de resultado com o contexto
+            return render(request, 'Simular_Emprestimo/resultado.html', context=context)
+    return render(request, 'Simular_Emprestimo/index.html')
+def index(request):
+    return render(request, 'Simular_Emprestimo/index.html')
